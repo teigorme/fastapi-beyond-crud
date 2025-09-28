42fb59fcec3e0e49bebe8145e7f62840041eb672
teigorme <igorteixeira354@gmail.com>
2025-09-28 17:18:52 +0100

    feat: add commet

--- src/main.py
+++ src/main.py
@@ -73,6 +73,7 @@ class Book(BaseModel):
     language: str
 
 
+# Validation data
 class UpdateBookModel(BaseModel):
     title: str
     author: str
@@ -86,24 +87,23 @@ class UpdateBookModel(BaseModel):
 async def get_all_books():
     return books
 
-
+# Create book
 @app.post("/books", response_model=Book, status_code=status.HTTP_201_CREATED)
 async def create_book(book_data: Book):
     new_book = book_data.model_dump()
     books.append(new_book)
     return new_book
 
-
+# Get a book
 @app.get("/books/{book_id}")
 async def get_book(book_id: int):
     for book in books:
         if book["id"] == book_id:
-
             return book
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-
+# Update book
 @app.patch("/books/{book_id}")
 async def update_book(book_id: int, book_update_data: UpdateBookModel):
     for book in books:
@@ -118,7 +118,7 @@ async def update_book(book_id: int, book_update_data: UpdateBookModel):
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-
+# Delete book
 @app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int):
     for book in books:
